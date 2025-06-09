b0d18e216d185e2def2ceba701e1234fa21b9133
Akash Tripathi <tripathig0542@gmail.com>
2025-06-09 16:56:30 +0530

    ok

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,7 @@ function NotFound() {
   return (
     <div className="not-found">
       <h2>404 - Page Not Found</h2>
-      <p>Sorry, the page you requested does not exist.</p>
+      <p>Sorry, the page you requested does not exist.Go to Home instead!</p>
     </div>
   );
 }
