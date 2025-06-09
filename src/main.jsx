056cc0a694b0161d182760bbdf3caf13efbf33bd
Akash Tripathi <tripathig0542@gmail.com>
2025-06-09 17:21:01 +0530

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 import store from './redux/store';
@@ -9,8 +9,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>
   </Provider>
 );
